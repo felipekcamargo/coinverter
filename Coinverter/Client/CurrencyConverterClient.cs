d50366c885302952da7664b523c2a31482895417
Felipe <felipe.camargo@vtex.com>
2021-10-19 21:04:05 -0300

    Included external call

---
+++ Coinverter/Client/CurrencyConverterClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Coinverter.Client
+{
+    public class CurrencyConverterClient
+    {
+
+        private readonly HttpClient _httpClient;
+
+        private CurrencyConverterClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task <string> GetCurrency()
+        {
+            using (var response = await _httpClient.GetAsync("https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=y&apiKey=b424d956f5173206e6fb"))
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            return "";
+        }
+        
+    }
+}
